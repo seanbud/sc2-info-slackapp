70b6f2f0f9ae45ba9e545b7d4e60ec16828354e0
seanbud <sbudning@gmail.com>
2019-06-03 02:04:31 -0400

    Added server.js as the launch point of the entire project.

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+const app = express();
+
+// Server front end.
+app.use(express.static(`${__dirname}/public`));
+
+// Handle all routes and behaviour of the app.
+app.use(require('./src/app.js'));
+
+// Start Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`starting server -- listening on port ${PORT}.\n`));
